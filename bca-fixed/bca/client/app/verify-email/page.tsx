467bdc484774239777e24a7e9d08931cbd79f793
beastcricketofficialauction <beastcricketofficialauction@gmail.com>
2026-04-01 15:05:57 +0530

    Update page.tsx

--- bca-fixed/bca/client/app/verify-email/page.tsx
+++ bca-fixed/bca/client/app/verify-email/page.tsx
@@ -1,10 +1,7 @@
-import { Suspense } from 'react';
 import VerifyEmailWrapper from './VerifyEmailWrapper';
+
 export const dynamic = 'force-dynamic';
+
 export default function Page() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <VerifyEmailWrapper />
-    </Suspense>
-  );
+  return <VerifyEmailWrapper />;
 }
