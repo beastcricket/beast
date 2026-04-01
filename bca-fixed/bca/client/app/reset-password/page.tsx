cac145a966618b0a6a474260b575dcdeac60426b
beastcricketofficialauction <beastcricketofficialauction@gmail.com>
2026-04-01 15:13:27 +0530

    Update page.tsx

--- bca-fixed/bca/client/app/reset-password/page.tsx
+++ bca-fixed/bca/client/app/reset-password/page.tsx
@@ -1,6 +1,7 @@
-import ResetPasswordWrapper from './ResetPasswordWrapper';
-
 export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+
+import ResetPasswordWrapper from './ResetPasswordWrapper';
 
 export default function Page() {
   return <ResetPasswordWrapper />;
