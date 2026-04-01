9e6ad0bd42fb1bc16c03e4f26398cbefeefeb430
beastcricketofficialauction <beastcricketofficialauction@gmail.com>
2026-04-01 14:45:36 +0530

    Update page.tsx

--- bca-fixed/bca/client/app/reset-password/page.tsx
+++ bca-fixed/bca/client/app/reset-password/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 import ResetPasswordWrapper from './ResetPasswordWrapper';
-
+export const dynamic = 'force-dynamic';
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
